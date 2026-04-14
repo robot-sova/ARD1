612fbd4877c6c583ed7aabd41b478e1b6094821e
Roman <roman@lgdryer.repair>
2026-04-14 15:05:08 -0700

    Fix site URL for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://friendly-squirrel-dc0a02.netlify.app',
+  site: 'https://appliancerepairdaily.com',
   integrations: [tailwind(), react(), sitemap()]
 });
